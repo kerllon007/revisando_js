d6c4e4f14e9fd38eeea78a7e52272b566f769ccd
kerllon <kerllon.mazzucatto2escola.pr.gov.br>
2024-03-15 12:13:23 -0300

    Subindo arquivo

---
+++ tipo-booleano.js
@@ -0,0 +1,10 @@
+let cidade= "Campo Largo";
+
+let cidade_natal = "campo largo";
+
+cidade = cidade.toLowerCase;
+
+cidade_natal = cidade_natal.toLowerCase
+
+console.log(cidade === cidade_natal);
+
